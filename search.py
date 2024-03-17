4309beb6257889465dd871d7b691d9569dbe404d
Qianchuli <qianchul1@student.unimelb.edu.au>
2024-03-17 13:51:21 +0800

    testing

--- search.py
+++ search.py
@@ -81,6 +81,7 @@ def depthFirstSearch(problem):
     print("Start's successors:", problem.getSuccessors(problem.getStartState()))
     """
     "*** YOUR CODE HERE ***"
+    print("testing")
 
     util.raiseNotDefined()
 
